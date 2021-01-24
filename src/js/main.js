fcc9e63c0826c067f233ed4bca826a3311b4e274
Amantay <amantay.sv@gmail.com>
2021-01-25 00:30:10 +0600

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,8 @@ $('ul.game-tabs__caption').on('click', 'li:not(.game-tabs__tab_active)', functio
       .addClass('game-tabs__tab_active').siblings().removeClass('game-tabs__tab_active')
       .closest('div.game-tabs__tabs').find('div.game-tabs__content').removeClass('game-tabs__content_active').eq($(this).index()).addClass('game-tabs__content_active');
   });
+$('ul.page__caption').on('click', 'li:not(.page__str_active)', function () {
+    $(this)
+      .addClass('page__str_active').siblings().removeClass('page__str_active')
+      .closest('div.game-tabs__tabs').find('div.page__content').removeClass('page__content_active').eq($(this).index()).addClass('page__content_active');
+  });
